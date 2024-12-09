c2c0dd538a2a3850d8698e98519b3d722627e9e6
bwondessen <bwondessen23@gmail.com>
2024-12-09 17:37:33 -0600

    Display categories underneath each post topic

--- script.js
+++ script.js
@@ -6,108 +6,109 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 const postsContainer = document.getElementById("posts-container");
 
 const allCategories = {
-    299: { category: "Career Advice", className: "career" },
-    409: { category: "Project Feedback", className: "feedback" },
-    417: { category: "freeCodeCamp Support", className: "support" },
-    421: { category: "JavaScript", className: "javascript" },
-    423: { category: "HTML - CSS", className: "html-css" },
-    424: { category: "Python", className: "python" },
-    432: { category: "You Can Do This!", className: "motivation" },
-    560: { category: "Backend Development", className: "backend" },
+  299: { category: "Career Advice", className: "career" },
+  409: { category: "Project Feedback", className: "feedback" },
+  417: { category: "freeCodeCamp Support", className: "support" },
+  421: { category: "JavaScript", className: "javascript" },
+  423: { category: "HTML - CSS", className: "html-css" },
+  424: { category: "Python", className: "python" },
+  432: { category: "You Can Do This!", className: "motivation" },
+  560: { category: "Backend Development", className: "backend" },
 };
 
 const forumCategory = (id) => {
-    let selectedCategory = {};
+  let selectedCategory = {};
 
-    if (allCategories.hasOwnProperty(id)) {
-        const { className, category } = allCategories[id];
+  if (allCategories.hasOwnProperty(id)) {
+    const { className, category } = allCategories[id];
 
-        selectedCategory.className = className;
-        selectedCategory.category = category;
-    } else {
-        selectedCategory.className = "general";
-        selectedCategory.category = "General";
-        selectedCategory.id = 1;
-    }
-    const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
-    const linkText = selectedCategory.category;
-    const linkClass = `category ${selectedCategory.className}`;
+    selectedCategory.className = className;
+    selectedCategory.category = category;
+  } else {
+    selectedCategory.className = "general";
+    selectedCategory.category = "General";
+    selectedCategory.id = 1;
+  }
+  const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
+  const linkText = selectedCategory.category;
+  const linkClass = `category ${selectedCategory.className}`;
 
-    return `<a href="${url}" class="${linkClass}" target="_blank">
+  return `<a href="${url}" class="${linkClass}" target="_blank">
     ${linkText}
   </a>`;
 };
 
 const timeAgo = (time) => {
-    const currentTime = new Date();
-    const lastPost = new Date(time);
+  const currentTime = new Date();
+  const lastPost = new Date(time);
 
-    const timeDifference = currentTime - lastPost;
-    const msPerMinute = 1000 * 60;
+  const timeDifference = currentTime - lastPost;
+  const msPerMinute = 1000 * 60;
 
-    const minutesAgo = Math.floor(timeDifference / msPerMinute);
-    const hoursAgo = Math.floor(minutesAgo / 60);
-    const daysAgo = Math.floor(hoursAgo / 24);
+  const minutesAgo = Math.floor(timeDifference / msPerMinute);
+  const hoursAgo = Math.floor(minutesAgo / 60);
+  const daysAgo = Math.floor(hoursAgo / 24);
 
-    if (minutesAgo < 60) {
-        return `${minutesAgo}m ago`;
-    }
+  if (minutesAgo < 60) {
+    return `${minutesAgo}m ago`;
+  }
 
-    if (hoursAgo < 24) {
-        return `${hoursAgo}h ago`;
-    }
+  if (hoursAgo < 24) {
+    return `${hoursAgo}h ago`;
+  }
 
-    return `${daysAgo}d ago`;
+  return `${daysAgo}d ago`;
 };
 
 const viewCount = (views) => {
-    const thousands = Math.floor(views / 1000);
+  const thousands = Math.floor(views / 1000);
 
-    if (views >= 1000) {
-        return `${thousands}k`;
-    }
+  if (views >= 1000) {
+    return `${thousands}k`;
+  }
 
-    return views;
+  return views;
 };
 
 const fetchData = async () => {
-    try {
-        const res = await fetch(forumLatest);
-        const data = await res.json();
-        showLatestPosts(data);
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const res = await fetch(forumLatest);
+    const data = await res.json();
+    showLatestPosts(data);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 fetchData();
 
 const showLatestPosts = (data) => {
-    const { topic_list, users } = data;
-    const { topics } = topic_list;
+  const { topic_list, users } = data;
+  const { topics } = topic_list;
 
-    postsContainer.innerHTML = topics.map((item) => {
-        const {
-            id,
-            title,
-            views,
-            posts_count,
-            slug,
-            posters,
-            category_id,
-            bumped_at,
-        } = item;
+  postsContainer.innerHTML = topics.map((item) => {
+    const {
+      id,
+      title,
+      views,
+      posts_count,
+      slug,
+      posters,
+      category_id,
+      bumped_at,
+    } = item;
 
-        return `
+    return `
     <tr>
       <td>
         <p class="post-title">${title}</p>
+        ${forumCategory(category_id)}
       </td>
       <td></td>
       <td>${posts_count - 1}</td>
       <td>${viewCount(views)}</td>
       <td>${timeAgo(bumped_at)}</td>
     </tr>`;
-    }).join("");
+  }).join("");
 };
 
